f1373ed0dabde62d8ec1b4f8aac00c5bf8cafa9f
Sandesh <sandeshdamkondwar@gmail.com>
2015-01-18 02:49:30 +0530

    Got it working

--- js/concurrent-tasks.js
+++ js/concurrent-tasks.js
@@ -1,8 +1,49 @@
 define([], function () {
-
-    var ParellelTasks = function () {
-
+    var SyncFuncs = function () {
+        this.functions = [];
+        this.isAllDone = false;
+        this.outputs = [];
+        this.pendingTasks = 0;
+        this.defaultTimeout = 3000;
     };
 
-    return ParellelTasks;
+    // Registering new synchronous callback
+    SyncFuncs.prototype.registerTask = function(callback) {
+        this.functions.push(callback);
+        this.pendingTasks++;
+    };
+
+    // Registeing the final task
+    SyncFuncs.prototype.registerFinalTask = function(callback) {
+        this.allDone = callback;
+    };
+
+    SyncFuncs.prototype.start = function() {
+        for (var i = 0;  i < this.functions.length; i++) {
+            this.functions[i](this);
+        }
+
+        this.timeout = this.timeout || this.defaultTimeout;
+
+        setTimeout(function () {
+            if(this.isAllDone === false) {
+                console.log('Timed Out .....');
+                this.isAllDone = true;
+                this.allDone(this.outputs);
+            }
+        }, this.timeout);
+    };
+
+    // This one will get called after every single task done
+    SyncFuncs.prototype.markOneDone = function(data) {
+        this.pendingTasks--;
+        this.outputs.push(data);
+
+        if (!this.isAllDone && this.pendingTasks === 0) {
+            this.allDone(this.outputs);
+            this.isAllDone = true;
+        }
+    };
+
+    return SyncFuncs;
 });
--- js/main.js
+++ js/main.js
@@ -1,2 +1,39 @@
-require(['jQuery', 'underscore', 'concurrent-tasks'], function($, _, ParellelTasks) {
+require(['jQuery', 'underscore', 'concurrent-tasks'], function($, _, SyncFuncs) {
+    var syncFuncs = new SyncFuncs();
+
+    syncFuncs.registerTask(function() {
+        setTimeout(function () {
+            console.log("Task 1 completed");
+            syncFuncs.markOneDone("Task 1 output");
+        }, 400);
+    });
+
+    syncFuncs.registerTask(function() {
+        setTimeout(function () {
+            console.log("Task 2 completed");
+            syncFuncs.markOneDone("Task 2 output");
+        }, 300);
+    });
+
+    syncFuncs.registerTask(function() {
+        setTimeout(function () {
+            console.log("Task 3 completed");
+            syncFuncs.markOneDone("Task 3 output");
+        }, 200);
+    });
+
+    syncFuncs.registerTask(function() {
+        setTimeout(function () {
+            console.log("Task 4 completed");
+            syncFuncs.markOneDone("Task 4 output");
+        }, 100);
+    });
+
+    syncFuncs.timout = 3000;
+
+    syncFuncs.registerFinalTask(function(outputs) {
+        console.log(outputs);
+    });
+
+    syncFuncs.start();
 });
